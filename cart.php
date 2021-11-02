7214bf113f442b7f5efddd501b520cbc9e35d129
Jonathan PERMAROLE <jonathan.permarole@hotmail.com>
2021-11-02 11:26:58 +0100

    Added what s need for the quest

--- cart.php
+++ cart.php
@@ -1,7 +1,17 @@
-<?php require 'inc/head.php'; ?>
+<?php
+session_start();
+require 'inc/head.php';
+require 'inc/data/products.php'; ?>
 <section class="cookies container-fluid">
     <div class="row">
-        TODO : Display shopping cart items from $_SESSION here.
+        <?php if (isset($_SESSION['cookies'])) {
+            $cookies = array_count_values($_SESSION['cookies']); ?>
+            <ul>
+                <?php foreach ($cookies as $id => $number) { ?>
+                    <li> <?= $catalog[$id]['name'] . " x" .  $number ?> </li>
+                <?php } ?>
+            </ul>
+        <?php } ?>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php require 'inc/data/products.php'; ?>
+<?php
+session_start();
+require 'inc/data/products.php';
+
+if (isset($_GET['add_to_cart'])) {
+    if (isset($_SESSION['cookies'])) {
+        array_push($_SESSION['cookies'], $_GET['add_to_cart']);
+    } else {
+        $_SESSION['cookies'] = array($_GET['add_to_cart']);
+    }
+}
+?>
 <?php require 'inc/head.php'; ?>
 <section class="cookies container-fluid">
     <div class="row">
@@ -9,9 +20,14 @@
                     <figcaption class="caption">
                         <h3><?= $cookie['name']; ?></h3>
                         <p><?= $cookie['description']; ?></p>
-                        <a href="?add_to_cart=<?= $id; ?>" class="btn btn-primary">
-                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
-                        </a>
+                        <?php if (isset($_SESSION['name'])) { ?>
+                            <a href="?add_to_cart=<?= $id; ?>" class="btn btn-primary">
+                            <?php } else { ?>
+                                <a href="login.php" class="btn btn-primary">
+                                <?php } ?>
+                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                                </a>
+
                     </figcaption>
                 </figure>
             </div>
